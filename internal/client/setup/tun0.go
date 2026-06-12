9273d1f45abb39aa0e4e97a6c0ae2217936626cb
john githiyon <johngithiyon7@gmail.com>
2026-06-12 13:43:32 +0530

    feat: write the func for the tun0 interface

---
+++ internal/client/setup/tun0.go
@@ -0,0 +1,21 @@
+package setup
+
+import (
+	"log"
+	"os/exec"
+)
+
+
+func Createtun0() error {
+
+	  _,runerr := exec.Command("ip", "tuntap", "add" ,"dev","tun0" ,"mode" ,"tun").CombinedOutput()
+
+       if runerr != nil {
+		 log.Println("runerr",runerr)
+		 return runerr
+	   }
+
+	   
+
+	  return nil 
+}
