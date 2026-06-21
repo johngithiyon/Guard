b7cd38982f3faeb9b82e6bbb16e1c5aabcf4ccdf
john githiyon <johngithiyon7@gmail.com>
2026-06-21 15:32:56 +0530

    feat: write the func to check the tun0 interface

---
+++ internal/client/setup/tun0exists.go
@@ -0,0 +1,22 @@
+package setup
+
+import "net"
+
+//Function to check the tun0 interface exists or not 
+
+func Tun0exists() error {
+ 
+	 // Ask the linux kernal for the list of interfaces on go check the tun0 interface
+	 
+	_,interfacerr := net.InterfaceByName("tun0")
+
+	   if interfacerr != nil {
+            createrr :=  Createtun0()
+			
+			if createrr != nil {
+                 return createrr
+			}
+	   } 
+
+       return nil 
+}
