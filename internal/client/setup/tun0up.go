e67d5cfa17ae56830a13f4e285bf9afbbb4d3d0a
john githiyon <johngithiyon7@gmail.com>
2026-06-21 15:43:31 +0530

    feat: write the func for the interface status up

---
+++ internal/client/setup/tun0up.go
@@ -0,0 +1,19 @@
+package setup
+
+import (
+	"log"
+	"os/exec"
+)
+
+func Tun0Upstatus() error {
+
+	   status,uperr := exec.Command("ip","link","set","tun0","up").CombinedOutput()
+
+       log.Println("Tun0 status",status)
+
+	   if uperr != nil {
+		    return uperr
+	   }
+
+	   return nil 
+}
