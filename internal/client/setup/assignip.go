40e4d9cc74ac227ce4f7add5e7782870bf930263
john githiyon <johngithiyon7@gmail.com>
2026-06-21 15:11:23 +0530

    feat: write the func to assign ip to the interface

---
+++ internal/client/setup/assignip.go
@@ -0,0 +1,22 @@
+package setup
+
+import (
+	"log"
+	"os/exec"
+)
+
+//Function to assign the ip to the tun0 interface
+
+func Assignip(ip string) error {
+
+	 ipassign,ipassignerr := exec.Command("ip","addr","add",ip+"/24","dev","tun0").CombinedOutput()
+
+	 log.Println(string(ipassign))
+
+	 if ipassignerr != nil {
+		 log.Println("Ip assign error",ipassignerr)
+		 return ipassignerr
+	 }
+
+	 return nil
+}
