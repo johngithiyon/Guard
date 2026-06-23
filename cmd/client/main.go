a22e8cdf417adc8809f650cf68fc3e9127cfa0e5
john githiyon <johngithiyon7@gmail.com>
2026-06-23 20:26:37 +0530

    feat: add the func call for create connection

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"log"
+
 	"github.com/johngithiyon/Guard/internal/client/services"
 	"github.com/johngithiyon/Guard/internal/client/setup"
 )
 
 func main() {
+
+	connerr := services.CreateConnection()
+
+	if connerr != nil {
+		 log.Println("Connection err",connerr)
+		 return
+	}
 
 	  existerr :=  setup.Tun0exists()
 
