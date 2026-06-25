6384cc3f7ba92b008c497924c07944b7c0aff166
john githiyon <johngithiyon7@gmail.com>
2026-06-25 21:02:05 +0530

    feat: write the func to load the env file

---
+++ internal/config/env.go
@@ -0,0 +1,18 @@
+package config
+
+
+import (
+	"log"
+	"github.com/joho/godotenv"
+)
+
+func Loadenv () error {
+   loaderr :=  godotenv.Load("internal/config/.env")
+
+   if loaderr != nil {
+	   log.Println("Load error",loaderr)
+	   return loaderr
+   }
+   
+   return nil 
+}
