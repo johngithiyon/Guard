3988253f573c581bc7885f0dac0e9cdde5b0c069
john githiyon <johngithiyon7@gmail.com>
2026-07-04 19:54:38 +0530

    fix: change the ip subnet range

--- internal/client/setup/route.go
+++ internal/client/setup/route.go
@@ -2,22 +2,17 @@ package setup
 
 import (
 	"log"
-	"os"
 	"os/exec"
 )
 
 //Function Used to create a entry in the kernal for request route to the tun0 interface
 
-func Route() error {
+func Route(ip string) error {
 
-	//Replace the ip with your ssh server private ip
-	
-	sship := os.Getenv("SSH_IP")
-
-	status,routerr := exec.Command("ip","route","add",sship,"dev","guard0").CombinedOutput()
+	status,routerr := exec.Command("ip","route","add",ip,"dev","guard0").CombinedOutput()
 
 	if routerr != nil {
-		log.Println("Status of route err",status)
+		log.Println("Status of route err",string(status))
 		return routerr
 	}
 
