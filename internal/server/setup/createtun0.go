6ea5c3df26cb7e012c532927db25d25c4d011a1f
john githiyon <johngithiyon7@gmail.com>
2026-07-04 19:46:47 +0530

    feat: write the func call for assignip

--- internal/server/setup/createtun0.go
+++ internal/server/setup/createtun0.go
@@ -8,30 +8,36 @@ import (
 
 //Function Used to create and write the received packets to the gurad-server interface
 
-func Createtun0() (*water.Interface,error) {
+func Createtun0() (*water.Interface, error) {
 
 	config := water.Config{
 		DeviceType: water.TUN,
-  }
+	}
 
-  //Name the tun interface as guard0
+	//Name the tun interface as guard0
 
-  config.Name = "guard-server"
+	config.Name = "guard-server"
 
-  // open the tun interface
+	// open the tun interface
 
-  tun,createrr := water.New(config)
+	tun, createrr := water.New(config)
 
-  if createrr != nil {
-	   log.Println("Create err",createrr)
-	   return nil,createrr
-  }
-  
-   uperr :=  Tun0Upstatus() 
+	if createrr != nil {
+		log.Println("Create err", createrr)
+		return nil, createrr
+	}
 
-   if uperr != nil {
-	    return nil,uperr
+   assignerr := Assignip()
+
+   if assignerr != nil {
+       return nil,assignerr
    }
 
-	return  tun,nil 
+	uperr := Tun0Upstatus()
+
+	if uperr != nil {
+		return nil, uperr
+	}
+
+	return tun, nil
 }
