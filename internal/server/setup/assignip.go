1cb626e49c44153b3a50e6bcd794b4298c1bdf86
john githiyon <johngithiyon7@gmail.com>
2026-07-04 19:45:49 +0530

    feat: write the func to assign the ip for tun0 interface

---
+++ internal/server/setup/assignip.go
@@ -0,0 +1,24 @@
+package serversetup
+
+import (
+	"log"
+	"os/exec"
+)
+
+//Function to assign the ip to the tun0 interface
+
+// I am just hardcode the ip address you can config any private ip range but it same to the client interface range
+
+func Assignip() error {
+
+	ipassign,ipassignerr := exec.Command("ip","addr","add","10.0.0.1","dev","guard-server").CombinedOutput()
+
+	log.Println(string(ipassign))
+
+	if ipassignerr != nil {
+		log.Println("Ip assign error",ipassignerr)
+		return ipassignerr
+	}
+
+	return nil 
+}	
