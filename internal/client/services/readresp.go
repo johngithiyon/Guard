599a91ca38837994440a64b372f610d06a394f71
john githiyon <johngithiyon7@gmail.com>
2026-07-04 19:29:33 +0530

    feat: read the resp and write in the tun interface

---
+++ internal/client/services/readresp.go
@@ -0,0 +1,25 @@
+package services
+
+import (
+	"log"
+
+	"github.com/songgao/water"
+)
+
+//Function Used to read the resp from the udp socket
+
+func Readresp(tun *water.Interface) error  {
+        
+	    buffer := make([]byte,1024)
+
+        length,readerr := Conn.Read(buffer)
+
+		if readerr != nil {
+			  log.Println("Read err in resp",readerr)
+			  return readerr
+		}
+
+		tun.Write(buffer[:length])
+
+	   return nil
+}
