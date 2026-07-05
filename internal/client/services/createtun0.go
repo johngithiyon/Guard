62129af26e0503587d5f18340ee81b7ba7122d35
john githiyon <johngithiyon7@gmail.com>
2026-07-05 11:32:21 +0530

    feat: add the go routine to the readtun0 func

---
+++ internal/client/services/createtun0.go
@@ -0,0 +1,43 @@
+package services
+
+import (
+	"log"
+
+	"github.com/johngithiyon/Guard/internal/client/setup"
+	"github.com/songgao/water"
+)
+
+//Function is used to read the IP packet from the tun0
+
+func Createtun0() error {
+
+	   //Creates a config object to tell the package I am using Tun 
+
+	   config := water.Config{
+		     DeviceType: water.TUN,
+	   }
+
+	   //Name the tun interface as guard0
+
+	   config.Name = "guard0"
+
+	   // open the tun interface
+
+	   tun,createrr := water.New(config)
+
+	   if createrr != nil {
+		    log.Println("Create err",createrr)
+		    return createrr
+	   }
+
+	   iperr := setup.ReqIpaddress(Conn)
+
+	   if iperr != nil {
+		   log.Println("iperr",iperr)
+		   return iperr 
+	   }
+    
+	   go  Readtun0(tun)
+
+	   return nil 
+}
