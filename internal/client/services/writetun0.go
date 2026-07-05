6f19525528dbac6d6cf6c119254c4af68841fae7
john githiyon <johngithiyon7@gmail.com>
2026-07-05 13:35:52 +0530

    feat: write the func for write packets in tun0 interface

---
+++ internal/client/services/writetun0.go
@@ -0,0 +1,21 @@
+package services
+
+import (
+	"log"
+
+	"github.com/songgao/water"
+)
+
+//Function Used to write the packets to the tun0 interface
+
+func Writetun0(tun *water.Interface,data []byte) error {
+
+	_,writerr := tun.Write(data)
+        
+	if writerr != nil {
+		 log.Println("Writerr in tun interface",writerr)
+		 return writerr
+	}
+	
+    return nil 
+}
