0607b6cd2cc5d74b58eec81543cfa093f692efbe
john githiyon <johngithiyon7@gmail.com>
2026-07-07 11:53:37 +0530

    fix: add the mtu limit to the tun0 interface

---
+++ internal/server/setup/mtulimit.go
@@ -0,0 +1,19 @@
+package serversetup
+
+import (
+	"log"
+	"os/exec"
+)
+
+func Mtulimit() error {
+
+	   status,mtuerr := exec.Command("ip","link","set","dev","guard-server","mtu","1472").CombinedOutput()
+	  
+	   if mtuerr != nil {
+		     log.Println("Mtu err",string(status),mtuerr)
+			 return mtuerr
+	   }
+
+	   return nil 
+
+}
