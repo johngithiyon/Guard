38330a660e82fa8f6b441737627728f026fb3d86
john githiyon <johngithiyon7@gmail.com>
2026-07-07 10:38:00 +0530

    debug: handle the partial packets err

--- internal/server/services/sendresp.go
+++ internal/server/services/sendresp.go
@@ -9,38 +9,46 @@ import (
 
 //Function Used to send the response hack to the client
 
-func Sendresp(conn net.PacketConn,tun *water.Interface) error {
+func Sendresp(conn net.PacketConn, tun *water.Interface) error {
 
-	buffer := make([]byte,1024)
+	buffer := make([]byte, 65535)
 
-	   // Read the resp from the tun0 interface
+	// Read the resp from the tun0 interface
 
-	   for {
-            
-		      length,readerr := tun.Read(buffer)
+	for {
 
-			  if readerr != nil {
-				   log.Println("Read err in tun interface",readerr)
-				   return readerr
-			  }
+		length, readerr := tun.Read(buffer)
 
-			  packets := buffer[:length]
+		if readerr != nil {
+			log.Println("Read err in tun interface", readerr)
+			return readerr
+		}
 
-			  //parse the destination ip from the packets
+		packets := buffer[:length]
 
-			  destip := net.IP(packets[16:20])
+		//parse the destination ip from the packets
 
-			  log.Println(destip)
+		destip := net.IP(packets[16:20])
 
-			 clientip :=  Allocated[destip.String()]
+		log.Println("destip", destip)
 
-			_,writerr :=  conn.WriteTo(packets,clientip)
+		clientip := Allocated[destip.String()]
 
-			if writerr != nil {
-				log.Println("Write err",writerr)
-				return writerr
-			}
-            
+		if clientip == nil {
+			continue
+		}
+
+		lendata, writerr := conn.WriteTo(packets, clientip)
+
+		if writerr != nil {
+			log.Println("Write err killer", writerr)
+			continue
+		}
+
+		if lendata != len(packets){
+		    log.Println("Partial Packets",lendata,len(packets))
 	   }
+
+	}
 
 }
