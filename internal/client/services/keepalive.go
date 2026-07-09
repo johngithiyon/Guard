177adfeacf0daf57d9f84197144ce51771027c91
john githiyon <johngithiyon7@gmail.com>
2026-07-09 21:15:12 +0530

    fix: add the loop to continue the ping req

--- internal/client/services/keepalive.go
+++ internal/client/services/keepalive.go
@@ -5,12 +5,16 @@ import "time"
 //Function Used to send the alive packets every 20 seconds to prove the client is alive
 
 func Keepalive()  {
+
+	for {
      
 	 //It wait for the 20 seconds
 
      time.Sleep(20 * time.Second)
 
 	 Conn.Write([]byte("Ping"))
+
+	} 
 	    
 }
 
