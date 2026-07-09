ecaad75903e1441884d36371ec70c4d6d955fde3
john githiyon <johngithiyon7@gmail.com>
2026-07-09 14:46:52 +0530

    feat: write the code to manage the heartbeat for the clients

--- internal/server/services/ipallocator.go
+++ internal/server/services/ipallocator.go
@@ -2,11 +2,14 @@ package serverservices
 
 import (
 	"net"
+	"time"
+
+	"github.com/johngithiyon/Guard/internal/server/models"
 )
 
 //centralized slice for the ip list
 
-var ips = []string{
+var Ips = []string{
 	   "10.0.0.2",
 	   "10.0.0.3",
 }
@@ -15,20 +18,32 @@ var ips = []string{
 
 var Allocated = make(map[string]net.Addr)
 
+//Store the heartbeat for the clients 
+
+var Clients = make(map[string]*models.Client)
+
 //Function for the ipallocation
 
 //Using queue metheod for the o(1) ip allocation
 
 func Ipallocator(conn net.PacketConn,addr net.Addr)  {
 
-	     if len(ips) !=0 {
+	     if len(Ips) !=0 {
 
-	     conn.WriteTo([]byte(ips[0]),addr)
+	     conn.WriteTo([]byte(Ips[0]),addr)
 
-		 Allocated[ips[0]] = addr
+		 Allocated[Ips[0]] = addr
 
-		 ips = ips[1:]
-	  
+		client := &models.Client{
+              Clientaddr: Ips[0],
+			  Lastseen: time.Now(),
+		}
+
+		//store the client udp address and give the value as models.client 
+
+		Clients[addr.String()] = client
+
+		Ips = Ips[1:]
 
 		 } else {
 			conn.WriteTo([]byte("Ip insufficient"),addr)
