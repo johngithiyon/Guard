6e86fcf468e54bff6823cf7fb6ac8735ed33c685
john githiyon <johngithiyon7@gmail.com>
2026-07-09 14:43:08 +0530

    feat: add the struct for the client info

---
+++ internal/server/models/client.go
@@ -0,0 +1,10 @@
+package models
+
+import "time"
+
+//Create a struct to store the client info
+
+type Client struct {
+       Clientaddr string //this clientaddr is vpn sever allocated ip 
+	   Lastseen time.Time   //time used to check the heartbeat of the client 
+}
