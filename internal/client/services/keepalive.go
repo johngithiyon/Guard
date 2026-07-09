1876485a7b70aef79a7393895a51d6b29e254202
john githiyon <johngithiyon7@gmail.com>
2026-07-09 14:42:29 +0530

    feat: write the func for the ping req to the server

---
+++ internal/client/services/keepalive.go
@@ -0,0 +1,16 @@
+package services
+
+import "time"
+
+//Function Used to send the alive packets every 20 seconds to prove the client is alive
+
+func Keepalive()  {
+     
+	 //It wait for the 20 seconds
+
+     time.Sleep(20 * time.Second)
+
+	 Conn.Write([]byte("Ping"))
+	    
+}
+
