60c4b25e53119f3483591ee45a13fa9a07e26b97
john githiyon <johngithiyon7@gmail.com>
2026-07-09 14:43:44 +0530

    write the func to check the client is alive or not

---
+++ internal/server/services/checkalive.go
@@ -0,0 +1,23 @@
+package serverservices
+
+import "time"
+
+//Function used to check and remove the disconnected client
+
+func Checkalive() {
+
+	       //Wait for every 5 seconds 
+	   
+	       time.Sleep(5 * time.Second)
+
+		   for _,client  := range Clients {
+
+			    //Set the timeout for the client is 30 seconds
+
+			    if time.Since(client.Lastseen) > 30 * time.Second {
+					    
+					   delete(Allocated,client.Clientaddr)
+					   Ips = append(Ips, client.Clientaddr)
+				}
+		   }
+}
