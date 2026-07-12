7cf1eb9852867b1c83a72e156b3038831b2cd7e4
john githiyon <johngithiyon7@gmail.com>
2026-07-12 12:49:17 +0530

    feat(config): write the func to Loadconfig file

---
+++ internal/client/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"os"
+
+	"github.com/johngithiyon/Guard/internal/client/models"
+	"gopkg.in/yaml.v3"
+)
+
+var Config models.Config
+
+func Loadconfig() error {
+
+	    readata,readerr := os.ReadFile("config.yaml")
+
+		if readerr != nil {
+			 return readerr
+		}
+       
+		unmarsherr := yaml.Unmarshal(readata,&Config)
+
+		if unmarsherr != nil {
+			return  unmarsherr
+		}
+
+	    return nil 
+}
