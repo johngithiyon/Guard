76ebd6a782292b62eb86116d50cb619831ff9643
john githiyon <johngithiyon7@gmail.com>
2026-07-12 12:51:21 +0530

    feat: replace the severip and serverport with config struct

--- internal/client/services/createconnection.go
+++ internal/client/services/createconnection.go
@@ -3,6 +3,8 @@ package services
 import (
 	"log"
 	"net"
+
+	"github.com/johngithiyon/Guard/internal/client/config"
 )
 
 var Conn net.Conn
@@ -13,7 +15,7 @@ func CreateConnection() error  {
 	 
 	   //make the conn object for Vpn server
 
-	   Conn,Connerr = net.Dial("udp",":8080")
+	   Conn,Connerr = net.Dial("udp",config.Config.VpnServer+":"+config.Config.VpnServerPort)
 
 	   if Connerr != nil {
 		     log.Println("Connection error when send packets",Connerr)
