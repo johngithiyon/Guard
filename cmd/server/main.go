add5a589b6658ce45214966166093e94ca8180ab
john githiyon <johngithiyon7@gmail.com>
2026-07-13 09:56:44 +0530

    refactor: make the func call for Run func to minimize the main code

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,53 +1,14 @@
 package main
 
-import (
-	"log"
-
-	serverconfig "github.com/johngithiyon/Guard/internal/server/config"
-	"github.com/johngithiyon/Guard/internal/server/services"
-	serversetup "github.com/johngithiyon/Guard/internal/server/setup"
-)
+import "github.com/johngithiyon/Guard/internal/client/app"
 
 func main() {
 
-	  loadconfigerr := serverconfig.Loadconfig()
+	  runerr := app.Run()
 
-	  if loadconfigerr != nil {
-		  log.Println("Load config err",loadconfigerr)
-		  return 
+	  if runerr != nil {
+		   return 
 	  }
-
-	  conn,connerr := serverservices.Createconnection()
-
-	  if connerr != nil {
-		   return
-	  }
-
-	  existerr :=  serversetup.Tun0exists()
-
-	  if existerr != nil {
-		    tun,createrr := serversetup.Createtun0()
-
-			if createrr != nil {
-                   return 
-			}
-
-			//Goroutine to receive the packets from the clients
-
-	    	go serverservices.PacketReceiver(conn,tun)
-
-			//Goroutine to send the response to the clients
-
-			go serverservices.Sendresp(conn,tun)
-
-			//Goroutine to check the client is them alive or not 
-
-			go serverservices.Checkalive()
-
-	  }
-
-	  //make the process keep alive 
-
-	  select{}
+	 
 
 }
