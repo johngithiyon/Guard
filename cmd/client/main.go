3b6de9b28698d737f596e5893abbc3fcc39e07aa
john githiyon <johngithiyon7@gmail.com>
2026-07-13 09:45:52 +0530

    refactor: make the func call for Run func to minimize the main code

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,41 +1,11 @@
 package main
 
-import (
-	"log"
-
-	"github.com/johngithiyon/Guard/internal/client/config"
-	"github.com/johngithiyon/Guard/internal/client/services"
-	"github.com/johngithiyon/Guard/internal/client/setup"
-)
+import "github.com/johngithiyon/Guard/internal/client/app"
 
 func main() {
+         runerr := app.Run()
 
-	loadconfigerr := config.Loadconfig()
-
-	if loadconfigerr != nil {
-		  log.Println("load config err",loadconfigerr)
-		  return  
-	}
-
-	connerr := services.CreateConnection()
-
-	if connerr != nil {
-		 log.Println("Connection err",connerr)
-		 return
-	}
-
-      existerr :=  setup.Tun0exists()
-
-	  if existerr != nil {
-		  createrr := services.Createtun0()
-
-		  if createrr != nil {
-			    log.Println("Create tun err",createrr)
-			    return
-		  }
-	  }
-     
-	  //make the process keep alive 
-
-	  select{}
+		 if runerr != nil {
+			   return
+		 }
 }
